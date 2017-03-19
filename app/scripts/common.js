2d3cfdce7f22c3fd541ddb32ec190c1b042f13c7
alekseyrudenko <4815162342>
2017-03-19 18:08:23 +0300

    fix host for svg file

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,18 +1,15 @@
 $(function () {
   var svg_path = '/svg/sprite.svg',
     cache_hash = Date.now(),
-    host = window.location.host,
+    host = window.location.href,
     div;
 
   return $.get(
-    host + svg_path + '?' + cache_hash,
+    host + svg_path + '?' + cache_hash
   )
     .done(function (data) {
       div = document.createElement('div');
       div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
       document.body.insertBefore(div.childNodes[0], document.body.childNodes[0]);
-    })
-    .fail(function(){
-
     })
 });
