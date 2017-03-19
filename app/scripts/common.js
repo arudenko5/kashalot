5762fd408bab7ff459b814d4d25e3f1ba6085c1a
alekseyrudenko <4815162342>
2017-03-19 13:53:18 +0300

    initial

---
+++ app/scripts/common.js
@@ -0,0 +1,18 @@
+$(function () {
+  var svg_path = '/svg/sprite.svg',
+    cache_hash = Date.now(),
+    host = window.location.host,
+    div;
+
+  return $.get(
+    host + svg_path + '?' + cache_hash,
+  )
+    .done(function (data) {
+      div = document.createElement('div');
+      div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+      document.body.insertBefore(div.childNodes[0], document.body.childNodes[0]);
+    })
+    .fail(function(){
+
+    })
+});
---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  console.log(12345);
+  $('.owl-carousel').owlCarousel();
+});
